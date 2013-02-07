f3645f10bcf30a4ed1169ee2909ae9802ae27149
PsychoGame <jajadekroon@gmail.com>
2013-02-07 09:23:43 +0100

    Samsung's Opensource 2.6.35.7 kernel updated to 2.6.35.14. Also the defconfig of my build has been added to the arch/arm/configs folder as bcm21553_totoro_defconfig. Changes in the config are that the kernel is compressed using lzma compression. The gzip compatibility of the ramdisk also has been disabled and replaced with an lzma compression which is much smaller (but adds a little performance loss only noticable at boot). Ext2 & Ext3 modules have been disabled, because the Ext4 kernel module which I included is backwards compatible with version 2 and 3. Also I added all standard available cpu governors (conservative, ondemand, userspace, powersave) on top of the bcm21553 and peformance governor. For now the added governors do not work correctly. I will fix this later on when I have the proper clock frequency's included.

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 PREEMPT Wed Feb 6 17:01:05 CET 2013"
+#define LINUX_COMPILE_TIME "17:01:05"
+#define LINUX_COMPILE_BY "jasper"
+#define LINUX_COMPILE_HOST "jasper-300E4A-300E5A-300E7A"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
